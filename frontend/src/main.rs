543be1ab9952e242c6bd4db9b4a94a9bd918ebeb
Simon Bourne <simonbourne@gmail.com>
2022-03-03 16:19:23 +0000

    Use silkenweb tauri integration

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -72,42 +72,8 @@ fn feed_bandit(food: &str, response: Mutable<String>) {
     });
 }
 
-// TODO: Everything below this should be defined with macros or in a
-// silkenweb_tauri crate.
+#[silkenweb_tauri::client_command(fallible)]
+async fn feed_bandit_the_cat(food: &str) -> Result<String, String>;
 
-// TODO: Define this in a silkenweb_tauri crate
-#[wasm_bindgen(inline_js = r#"
-    export async function invoke(name, args) {
-        return await window.__TAURI__.invoke(name, args);
-    }
-"#)]
-
-extern "C" {
-    #[wasm_bindgen(catch)]
-    async fn invoke(name: String, args: JsValue) -> Result<JsValue, JsValue>;
-}
-
-// TODO: Generate this with a macro
-async fn feed_bandit_the_cat(food: &str) -> Result<String, String> {
-    // TODO: Macro to wrap `invoke` in function
-    let args = Object::new();
-
-    Reflect::set(&args, &"food".into(), &JsValue::from_serde(food).unwrap()).unwrap();
-
-    invoke("feed_bandit_the_cat".to_string(), args.into())
-        .await
-        .map(|ok| ok.into_serde().unwrap())
-        .map_err(|e| e.into_serde().unwrap())
-}
-
-// TODO: Generate this with a macro
-async fn feed_bandit_with_sardines() -> String {
-    // TODO: Macro to wrap `invoke` in function
-    let args = Object::new();
-
-    invoke("feed_bandit_with_sardines".to_string(), args.into())
-        .await
-        .unwrap()
-        .into_serde()
-        .unwrap()
-}
+#[silkenweb_tauri::client_command(infallible)]
+async fn feed_bandit_with_sardines() -> String;
