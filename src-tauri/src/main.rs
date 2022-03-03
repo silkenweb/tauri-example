099f3f6887d3189271634b8084d05edec15b990a
Simon Bourne <simonbourne@gmail.com>
2022-03-03 17:53:27 +0000

    Top down item order

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,16 @@
     windows_subsystem = "windows"
 )]
 
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            feed_bandit_the_cat,
+            feed_bandit_with_sardines
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
+
 #[tauri::command]
 fn feed_bandit_the_cat(food: &str) -> Result<String, String> {
     match food {
@@ -14,14 +24,4 @@ fn feed_bandit_the_cat(food: &str) -> Result<String, String> {
 #[tauri::command]
 fn feed_bandit_with_sardines() -> String {
     "More...".to_string()
-}
-
-fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-            feed_bandit_the_cat,
-            feed_bandit_with_sardines
-        ])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
 }
